9e213de18441168f84184720a9583c06de8a4c42
mumair <mumairofficial@gmail.com>
2018-11-27 13:22:16 +0500

    tooltip directive

--- src/app/shared/directives/tooltip.directive.ts
+++ src/app/shared/directives/tooltip.directive.ts
@@ -1,8 +1,17 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, Input, Renderer, AfterViewInit } from '@angular/core';
 
 @Directive({
   selector: '[tooltip]'
 })
-export class TooltipDirective {
-  // code here
+export class TooltipDirective implements AfterViewInit {
+  el: ElementRef;
+  @Input() tooltip;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer) {
+  }
+
+  ngAfterViewInit() {
+    this.renderer.setElementAttribute(this.elementRef.nativeElement, 'data-toggle', 'tooltip');
+    this.renderer.setElementAttribute(this.elementRef.nativeElement, 'data-title', this.tooltip);
+  }
 }
