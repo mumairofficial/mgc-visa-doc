c36eabb6df9d4e03308753eb05a6f2ba7d52e621
mumair <mumairofficial@gmail.com>
2018-11-27 16:34:59 +0500

    tooltip example

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
 import { BasicLayoutComponent } from './layout/basic-layout.component';
 import { BlankLayoutComponent } from './layout/blank-layout.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule,SharedModule, RouterModule],
   declarations: [
     LeftNavigationComponent,
     BasicLayoutComponent,
