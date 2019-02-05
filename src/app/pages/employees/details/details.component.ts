d1a628685665e075b71527410b77e780a2e8e331
mumair <mumairofficial@gmail.com>
2019-02-05 14:32:55 +0500

    formatting

--- src/app/pages/employees/details/details.component.ts
+++ src/app/pages/employees/details/details.component.ts
@@ -1,27 +1,26 @@
-import {Component} from '@angular/core';
-import {BreadCrumb} from '@shared/models';
-import {ActivatedRoute} from '@angular/router';
+import { Component } from "@angular/core";
+import { BreadCrumb } from "@shared/models";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-employee-details',
-  templateUrl: 'details.component.html'
+  selector: "app-employee-details",
+  templateUrl: "details.component.html"
 })
-
 export class DetailsComponent {
   breadCrumb: Array<BreadCrumb> = [
     {
-      title: 'Overview',
-      url: '/overview',
+      title: "Overview",
+      url: "/overview",
       active: false
     },
     {
-      title: 'Employees',
-      url: '/employees',
+      title: "Employees",
+      url: "/employees",
       active: false
     },
     {
-      title: 'John Doe',
-      url: '/employees/johndoe',
+      title: "John Doe",
+      url: "/employees/johndoe",
       active: true
     }
   ];
